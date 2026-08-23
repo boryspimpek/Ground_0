2cdf8309d96468aa64fe19134d5b194b414b3263
boryspimpek <borysgame@gmail.com>
2026-08-23 16:47:41 +0200

    sounds

--- Player/Components/GrenadeThrower.gd
+++ Player/Components/GrenadeThrower.gd
@@ -5,10 +5,14 @@ class_name GrenadeThrower
 @export var throw_marker: Marker3D
 
 @onready var player: CharacterBody3D = get_parent()
+@onready var animation_tree: AnimationTree = player.get_node("AnimationTree")
+
+const ANIM_THROW_SHOT = "parameters/ThrowShot/request"
+const THROW_COOLDOWN := 0.5
 
 var _current_count: int
 var _cooldown_timer: float = 0.0
-const THROW_COOLDOWN := 0.5
+
 
 func _ready() -> void:
 	_current_count = grenade_data.max_count
@@ -20,17 +24,26 @@ func update(delta: float) -> void:
 
 	if Input.is_action_just_pressed("throw") and _cooldown_timer <= 0.0:
 		print("Throwing grenade")
-		_throw()
+		_request_throw()
 
-func _throw() -> void:
+func _request_throw() -> void:
 	if _current_count <= 0:
+		print("Brak granatów!")
 		return
 	if not grenade_data.grenade_scene:
 		push_warning("Brak grenade_scene w GrenadeData!")
 		return
 
-	_current_count -= 1
 	_cooldown_timer = THROW_COOLDOWN
+	animation_tree.set(ANIM_THROW_SHOT, AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+
+# Wołane z Call Method Track w animacji rzutu, w klatce gdzie dłoń puszcza granat.
+func release_grenade() -> void:
+	if _current_count <= 0:
+		return
+
+	_current_count -= 1
 	EventBus.grenade_count_changed.emit(_current_count, grenade_data.max_count)
 
 	var grenade: Node = grenade_data.grenade_scene.instantiate()
@@ -42,5 +55,3 @@ func _throw() -> void:
 
 	grenade.setup(grenade_data, player)
 	grenade.apply_central_impulse(throw_dir * grenade_data.throw_force)
-
-	print("Grenade thrown! Current count: %d" % _current_count)
