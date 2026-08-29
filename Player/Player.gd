2a5ef247d03eed4674126ab1d98fa82699dfe9b0
boryspimpek <borysgame@gmail.com>
2026-08-29 13:13:26 +0200

    barrel phisics

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@export var push_force: float = 1.5 # Mnożnik siły popychania
+
 @onready var movement: Node = $Movement
 @onready var traversal: Node = $Traversal
 @onready var combat: Node = $Combat
@@ -20,6 +22,26 @@ func _physics_process(delta: float) -> void:
 	grenade_thrower.update(delta)
 
 	move_and_slide()
+	_handle_push_collisions()
 
 func take_damage(amount: float, source: Node = null) -> void:
 	health.take_damage(amount, source)
+
+func _handle_push_collisions() -> void:
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+
+		if collider is RigidBody3D:
+			# Kierunek pchnięcia (od odwrotności wektora normalnego kolizji)
+			var push_dir: Vector3 = -collision.get_normal()
+			
+			# Ignorujemy siłę w pionie (żeby gracz nie popychał ziemi w dół ani nie podbijał obiektów skacząc na nie)
+			push_dir.y = 0.0
+			push_dir = push_dir.normalized()
+
+			# Miejsce przyłożenia siły (offset od środka masy beczki)
+			var hit_offset: Vector3 = collision.get_position() - collider.global_position
+
+			# Przykładamy impuls do RigidBody3D
+			collider.apply_impulse(push_dir * push_force, hit_offset)
